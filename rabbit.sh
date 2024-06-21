270b766fd5d7cfc4af36ffe475b2014d6d972e1b
Jose Massigoge <jmmassigoge@gmail.com>
2024-06-21 10:11:37 -0300

    Chapter 1

--- rabbit.sh
+++ rabbit.sh
@@ -5,9 +5,17 @@ case "$1" in
         echo "Starting RabbitMQ container..."
         docker run -d --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
         ;;
+    stomp)
+        echo "Starting RabbitMQ container with STOMP plugin..."
+        docker run -d --rm --name rabbitmq-stomp -p 61613:61613 -p 15672:15672 rabbitmq-stomp
+        ;;
     stop)
         echo "Stopping RabbitMQ container..."
         docker stop rabbitmq
+        ;;
+    stop-stomp)
+        echo "Stopping RabbitMQ container with STOMP plugin..."
+        docker stop rabbitmq-stomp
         ;;
     logs)
         echo "Fetching logs for RabbitMQ container..."
