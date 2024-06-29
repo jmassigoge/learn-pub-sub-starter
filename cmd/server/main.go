31ca2a65bcd23fbe6fe5ffab278791f9dace4e16
Jose Massigoge <jmmassigoge@gmail.com>
2024-06-29 12:56:40 -0300

    Ende ch 3

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/jmassigoge/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/jmassigoge/learn-pub-sub-starter/internal/pubsub"
+	"github.com/jmassigoge/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -18,6 +21,52 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Peril game server connected to RabbitMQ!")
+	_, _, err = pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilTopic,
+		routing.GameLogSlug,
+		routing.GameLogSlug+".*",
+		pubsub.SimpleQueueDurable,
+	)
+	if err != nil {
+		log.Fatalf("could not create and bind queue: %v", err)
+	}
+	publishChannel, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("could not open channel: %v", err)
+	}
+
+	gamelogic.PrintServerHelp()
+	for {
+		userInput := gamelogic.GetInput()
+		if len(userInput) == 0 {
+			continue
+		}
+		if userInput[0] == "pause" {
+			log.Println("Sending pause message")
+			err = pubsub.PublishJSON(publishChannel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: true,
+			})
+			if err != nil {
+				log.Fatalf("could not publish message: %v", err)
+			}
+			continue
+		}
+		if userInput[0] == "resume" {
+			log.Println("Sending resume message")
+			err = pubsub.PublishJSON(publishChannel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: false,
+			})
+			if err != nil {
+				log.Fatalf("could not publish message: %v", err)
+			}
+			continue
+		}
+		if userInput[0] == "quit" {
+			break
+		}
+		log.Printf("Did not understand command")
+	}
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
---
+++ internal/pubsub/pubsub.go
@@ -0,0 +1,66 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Acktype int
+
+type SimpleQueueType int
+
+const (
+	SimpleQueueDurable SimpleQueueType = iota
+	SimpleQueueTransient
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	dat, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+	return ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+		ContentType: "application/json",
+		Body:        dat,
+	})
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not create channel: %v", err)
+	}
+
+	queue, err := ch.QueueDeclare(
+		queueName,                             // name
+		simpleQueueType == SimpleQueueDurable, // durable
+		simpleQueueType != SimpleQueueDurable, // delete when unused
+		simpleQueueType != SimpleQueueDurable, // exclusive
+		false,                                 // no-wait
+		nil,                                   // arguments
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not declare queue: %v", err)
+	}
+
+	err = ch.QueueBind(
+		queue.Name, // queue name
+		key,        // routing key
+		exchange,   // exchange
+		false,      // no-wait
+		nil,        // args
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not bind queue: %v", err)
+	}
+	return ch, queue, nil
+}
